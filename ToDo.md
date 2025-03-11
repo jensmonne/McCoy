b6da07054aa6c39ed25b85ba1983838ae3886a1e
jens <jensmonne@gmail.com>
2025-03-11 13:22:31 +0100

    js start

---
+++ ToDo.md
@@ -0,0 +1,14 @@
+Daily Streaks
+Level Leaderboard
+wordcount
+vc levels
+vc + text levels
+logging?
+birthday
+vc logger
+daily questions
+achievements
+join/quit logging
+level roles
+ping list
+quotes
--- src/index.js
+++ src/index.js
@@ -1,18 +1,51 @@
-const dotenv = require('dotenv');
+import { Client, Collection, GatewayIntentBits } from 'discord.js';
+import fs from 'node:fs';
+import path from 'node:path';
 
-dotenv.config();
-
-// Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require('discord.js');
-
-// Get the token from the environment variables
+// Get the token from the .env file
 const token = process.env.DISCORD_TOKEN;
+const prefix = '$';
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+client.commands = new Collection();
 
-client.once(Events.ClientReady, readyClient => {
-    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+const commandPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = await import(path.join(commandPath, file));
+    client.commands.set(command.data.name, command);
+}
+
+const eventPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+    const event = await import(path.join(eventPath, file));
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args, client));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args, client));
+    }
+}
+
+client.on('messageCreate', async message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+    const command = client.commands.get(commandName);
+
+    if (!command) return;
+
+    try {
+        await command.execute(message, args);
+    } catch (error) {
+        console.error(error);
+        await message.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    }
 });
 
 // Log in to Discord with your client's token
