d3fdefebba474eacd639d8adef9a7f43ff70fba7
quinVanGestel <quinvangestel@gmail.com>
2025-05-29 22:42:15 +0200

    Update Program.cs

--- McCoy/Program.cs
+++ McCoy/Program.cs
@@ -39,9 +39,24 @@ class Program
     {
         if (message.Author.IsBot) return;
 
-        if (message.Content == "!fart")
+        var allowUpper = message.Content.ToLower();
+        
+
+        if (allowUpper == "!fart")
         {
             await message.Channel.SendMessageAsync("FART!");
         }
+
+        if (allowUpper == "hi")
+        {
+            await message.Channel.SendMessageAsync(
+                "My name is Walter Hartwell White. I live at 308 Negra Arroyo Lane, Albuquerque, New Mexico, 87104. To all law enforcement entities, this is not an admission of guilt. I am speaking to my family now. Skyler, you are the love of my life. I hope you know that. Walter Jr., you're my big man. There are going to be some things that you'll come to learn about me in the next few days. But just know that no matter how it may look, I only had you in my heart. Goodbye.");
+        }
+
+        if (allowUpper == "ping")
+        {
+            await message.Channel.SendMessageAsync("Pong!");
+        }
+        
     }
 }
