556c0643e5e3021f6ad283f5c090418455811418
jens <jensmonne@gmail.com>
2025-05-29 21:49:25 +0200

    C# base

---
+++ McCoy/Program.cs
@@ -0,0 +1,33 @@
+﻿using Discord;
+using Discord.WebSocket;
+using DotNetEnv;
+
+class Program
+{
+    private static DiscordSocketClient? _client;
+
+    // public static Task Main(string[] args) => new Program().MainAsync();
+
+    public static async Task Main()
+    {
+        _client = new DiscordSocketClient();
+        
+        _client.Log += Log;
+        
+        Env.Load();
+
+        var token = Environment.GetEnvironmentVariable("DISCORD_TOKEN")
+            ?? throw new InvalidOperationException("DISCORD_TOKEN is not set.");
+        
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+        
+        await Task.Delay(-1);
+    }
+
+    private static Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+}
