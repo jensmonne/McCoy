b24170481fc93790d3defd4f1cb5d2be928a2505
quinVanGestel <quinvangestel@gmail.com>
2025-05-29 23:06:32 +0200

    Create MPreg.cs

---
+++ McCoy/functionalities/MPreg.cs
@@ -0,0 +1,18 @@
+﻿using Discord;
+
+namespace McCoy.functionalities;
+
+public class MPreg
+{
+    private GayMPreg()
+    {
+        if (Lorem.Content == gay)
+        {
+            reaction == :pregnant_man:
+                var MalePregnancy = Emote.Parse("<:pregnant_man:>");
+        }
+
+
+    }
+
+}
