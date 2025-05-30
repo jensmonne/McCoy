15ec71cd80fd7597e8dde4e32fdcbc6179cfa117
quinVanGestel <quinvangestel@gmail.com>
2025-05-30 13:31:02 +0200

    Update Impregnate.cs

--- McCoy/Handlers/Messages/Impregnate.cs
+++ McCoy/Handlers/Messages/Impregnate.cs
@@ -5,10 +5,15 @@ namespace McCoy.Handlers.Messages;
 
 public class Impregnate
 {
-    public static void Impregnation(SocketMessage message)
+    public static void Impregnation(SocketMessage message, bool isGay)
     {
         // Gary it already checks if the word "gay" is said or if it is chloe who says the message (:
         var MalePregnancy = new Emoji("🫃");
         message.AddReactionAsync(MalePregnancy);
+        
+        if (message, bool isGay)
+        {
+            message.AddReactionAsync(MalePregnancy);
+        }
     }
 }
