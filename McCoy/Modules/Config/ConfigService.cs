d74a178ffce5ce090864764e6f1e83168f9962b5
jens <jensmonne@gmail.com>
2025-06-06 13:53:02 +0200

    Better voice logging
    V0.1.3

--- McCoy/Modules/Config/ConfigService.cs
+++ McCoy/Modules/Config/ConfigService.cs
@@ -2,7 +2,7 @@
 
 public static class ConfigService
 {
-    public const string BotVersion = "0.1.2";
+    public const string BotVersion = "0.1.3";
     
     public static readonly ulong[] DevGuilds = { 1316537594850443304, 1145362546224799806 };
 }
